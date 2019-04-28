f08c21c13da58ba0007a9e89dfdf7b1e76dba6bf
linfan <276191344@qq.com>
2019-04-28 17:28:11 +0800

    output manage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+import _ from "lodash"
+import './style.css'
+import printMe from './print.js'
+
 function component() {
     var element = document.createElement('div');
+    var button = document.createElement('button');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    button.innerHTML = 'Click Me';
+    button.onclick = printMe;
+    element.appendChild(button);
   
     return element;
   }
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    // entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    output: {
+        // filename: 'bundle.js',
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
+};
