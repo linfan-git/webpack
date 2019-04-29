d0eb4767d334b6379828776744fc7b8aba5e9632
linfan <276191344@qq.com>
2019-04-29 16:41:30 +0800

    no description

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function component() {
     var button = document.createElement('button');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello'], ' ');
     element.classList.add('hello');
 
     button.innerHTML = 'Click Me';
