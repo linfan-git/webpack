31ab11a8fdef8468d8f64bb88f72c68ad41deb8d
linfan <276191344@qq.com>
2019-05-05 09:58:47 +0800

    add changes

--- src/print.js
+++ src/print.js
@@ -1,4 +1,5 @@
+let a = 1;
 export default function printMe() {
     // console.log('I get called from print.js!');
-    console.error('I get called from print.js!');
+    console.error('I get called from print.js:' + (a++));
 }
